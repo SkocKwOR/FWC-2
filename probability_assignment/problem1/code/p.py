a93832fef662e966129b7837a21ad0caac8d851e
SkocKwOR <112775196+SkocKwOR@users.noreply.github.com>
2022-12-30 11:20:14 +0530

    Update p.py

--- probability_assignment/problem1/code/p.py
+++ probability_assignment/problem1/code/p.py
@@ -1,13 +1,14 @@
 import numpy as np
 #rng = np.random.default_rng(12345)
+#simulation length
 s = 10000
-ps = 10/10000
-qs = 1-ps
-n= int(input())
+ps = 10/10000 #probability of wining with one ticket
+qs = 1-ps     #probability of losing with one ticket
+n= int(input()) #number of tickets
 count = 0
 for i in range(s):
     trail = np.random.choice([0,1],n,p=[qs,ps])
     if(np.count_nonzero(trail)>0):
         count +=1
 prob = count/s
-print(1-prob)
+print(1-prob) #total probability of losing the lottery
