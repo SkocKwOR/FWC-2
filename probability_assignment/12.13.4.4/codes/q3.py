b5cd65332bf6cf3fba6ef36376a298078ca3931f
SkocKwOR <112775196+SkocKwOR@users.noreply.github.com>
2023-01-05 19:25:55 +0530

    Add files via upload

---
+++ probability_assignment/12.13.4.4/codes/q3.py
@@ -0,0 +1,13 @@
+from math import comb
+
+dist=[]
+p = 1/2
+q = 1/2
+n = 4
+flRange = range(n+1)
+
+for i in flRange:
+    val = comb(n,i)*(p**i)*(q**(n-i))
+    dist.append(round(val,n))
+
+print(dist)
